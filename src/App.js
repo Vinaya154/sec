6d8b7d07ec8a1f6039de2cbc74d172220bb894cc
Vinaya154 <132821886+Vinaya154@users.noreply.github.com>
2024-01-29 21:47:30 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import {Route, Link, Routes, BrowserRouter} from 'react-router-dom';
 import Checkout from "./Checkout";
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Login"
+import {auth} from "./firebase";
+import { useStateValue } from "./StateProvider";
+import Orders from "./Orders";
 
 function App() {
-return (
-  <Router>
-  <div className="app">
-     <Routes>
-        <Route path="/checkout">
-          <Header />
-          <Checkout />
-          </Route>
-          <Route path="/">
-            <Header />
-          <Home />
-        </Route>
-      </Routes>
-    </div>
-    </Router>
-);
+  const [{},dispatch] = useStateValue();
+  useEffect(() => {
+    
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+       dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+  return (
+    <BrowserRouter>
+      <div className="app">
+      
+      
+      
+      
+          <Routes>
+          <Route path="/" element={<Home />} />
+          <Route exact path="/header" element={<Header />} />
+         <Route path="/home" element={<Home />} />
+         <Route path="/login" element={<Login />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+       </div>
+      </BrowserRouter>
+  );
 }
+
+
+
 export default App;
